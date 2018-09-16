cd0fe2bcec989d4b311095127c3439d53cb008c7
KavyaKPuranik <kpuranik08@gmail.com>
2018-09-16 22:29:08 +0530

    print and test

--- app.py
+++ app.py
@@ -74,7 +74,8 @@ def processInfo(req):
 # ----------------------------------------json data extraction functions---------------------------------------------------
 
 def makeWebhookResult(data):
-    fulfillmentText = data[0][word]
+    fulfillmentText = data[0]['word']
+    print('text: '+ fulfillmentText)
     #if data.get('response_code') == 210:
      #   speech = "Train may be cancelled or is not scheduled to run"
     return {
